c22a286157a99e526967c997b83f9342dcc6f534
warmdaylight <settasit@kaist.ac.kr>
2018-07-08 16:33:31 +0700

    draft of generate map

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -16,13 +16,55 @@ export class Game extends Component {
         }
     }
 
+    // helper function
+    /**
+     * 
+     * @param {number[]} position
+     * @returns positions of the coordinate neighbor
+     */
+    private getNeighbor(position) {
+        let x = position[0];
+        let y = position[1];
+
+        return [[x, y - 1], [x, y + 1], [x - 1, y], ]
+    }
+
+    private isSamePostion(a, b) {
+        return a[0] == b[0] && a[1] == b[1];
+    }
+
     /**
      * generate a map
      * @param {number} startX start position x
      * @param {number} startY start position y
      */
     generateMap(startX, startY) {
+        let placedBomb = [];
+        const start = [startX, startY];
+
+        // generate bomb
+        const bombDrop = (range) => Math.floor(Math.random() * range);
+        const isSamePostion = (a, b) => a[0] == b[0] && a[1] == b[1];
+
+        let explosiveMap = this.state.explosiveMap.map((e) => e.slice());
         
+
+        while(placedBomb.length <= this.state.explosive) {
+            // get coordinate
+            let position = [bombDrop(this.explosiveMap.length), bombDrop(this.explosiveMap[0].length)];
+
+            if (!isSamePostion(position, start) && !placedBomb.some((e) => isSamePostion(e, position))) {
+                placedBomb.push(position);
+                explosiveMap[position[0], position[1]] = -1;
+            }
+        }
+        // mark the numbers
+
+        for (var i = 0; i < explosiveMap.length; i++) {
+            for (var j = 0; j < explosiveMap[0].length; j++) {
+                
+            }
+        }
     }
 
     /**
@@ -35,9 +77,6 @@ export class Game extends Component {
         if (!this.state.explosiveMap[x][y]) {
             this.generateMap(x, y);
         }
-
-        
-        
     } 
 
     render() {
@@ -47,6 +86,4 @@ export class Game extends Component {
             </table>
         );
     }
-
-    
 }
